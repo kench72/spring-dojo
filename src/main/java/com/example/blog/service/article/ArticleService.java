b3988c67bcff45cab477e6a83a9ba069df4ad892
kench <77647366+kench72@users.noreply.github.com>
2024-03-22 06:47:54 +0900

    DI

--- src/main/java/com/example/blog/service/article/ArticleService.java
+++ src/main/java/com/example/blog/service/article/ArticleService.java
@@ -1,9 +1,12 @@
 package com.example.blog.service.article;
 
+import org.springframework.stereotype.Service;
+
 import javax.swing.text.html.Option;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+@Service
 public class ArticleService {
 
     public Optional<ArticleEntity> findById(long id) {

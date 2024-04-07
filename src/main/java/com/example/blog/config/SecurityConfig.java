f2025ed6465ec420b97dc15b191135e7aa7b7731
kench <77647366+kench72@users.noreply.github.com>
2024-04-07 15:07:36 +0900

    SessionAuthenticationStrategyのBean登録
    SessionAuthenticationStrategy（IF）をChangeSessionIdAuthenticationStrategyの型でBean登録。

--- src/main/java/com/example/blog/config/SecurityConfig.java
+++ src/main/java/com/example/blog/config/SecurityConfig.java
@@ -12,6 +12,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.session.ChangeSessionIdAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.context.SecurityContextRepository;
 
@@ -23,10 +25,15 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(
             HttpSecurity http
             , SecurityContextRepository securityContextRepository
+            , SessionAuthenticationStrategy sessionAuthenticationStrategy
     ) throws Exception {
         http
                 .csrf(csrf -> csrf.ignoringRequestMatchers("/login"))
-                .addFilterAt(new JsonUsernamePasswordAuthenticationFilter(securityContextRepository),
+                .addFilterAt(
+                        new JsonUsernamePasswordAuthenticationFilter(
+                                securityContextRepository
+                                , sessionAuthenticationStrategy
+                        ),
                         UsernamePasswordAuthenticationFilter.class)
                 .securityContext(context -> context.securityContextRepository(securityContextRepository))
                 .authorizeHttpRequests((authorize) -> authorize
@@ -39,8 +46,13 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityContextRepository securityContextRepository(){
+    public SecurityContextRepository securityContextRepository() {
         return new HttpSessionSecurityContextRepository();
+    }
+
+    @Bean
+    public SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new ChangeSessionIdAuthenticationStrategy();
     }
 
     @Bean

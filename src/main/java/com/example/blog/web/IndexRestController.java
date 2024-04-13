8089df9ae0b9ffc493b3d85d90b4006ca38a7004
kench <77647366+kench72@users.noreply.github.com>
2024-04-13 17:14:31 +0900

    Indexコントローラー作成
    ClientにCSRFトークンを渡すために認証スルーのEndpointを用意。

---
+++ src/main/java/com/example/blog/web/IndexRestController.java
@@ -0,0 +1,18 @@
+package com.example.blog.web;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexRestController {
+
+    // GET localhost:8080/
+    @GetMapping
+    public ResponseEntity<Void> index() {
+        return ResponseEntity
+                .noContent()
+                .build();
+    }
+}

7905af66cbd6aaf6107d06a219d997804a5cd229
kench <77647366+kench72@users.noreply.github.com>
2024-05-18 09:27:51 +0900

    トップページへのアクセスをテスト。

--- src/test/java/com/example/blog/it/RegistrationAndLoginIT.java
+++ src/test/java/com/example/blog/it/RegistrationAndLoginIT.java
@@ -3,7 +3,10 @@ package com.example.blog.it;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseCookie;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,7 +25,32 @@ public class RegistrationAndLoginIT {
 
     @Test
     public void integrationTest() {
-        assertThat(webTestClient).isNotNull();
+        // ## Arrange ##
+
+        // ## Act ##
+        var responseSpec = webTestClient.get().uri("/").exchange();
+
+        // ## Assert ##
+        // webFluxが提供する検証処理（assertメソッドに相当）。
+        responseSpec.expectStatus().isNoContent();
+
+        var response = responseSpec.returnResult(String.class);
+
+        // getFirstは@nullable付いてるのでnullを返却する可能性がある ⇒ こう言う時はoptionalを利用。
+        Optional<ResponseCookie> xsrfTokenCookieOpt = Optional.ofNullable(response.getResponseCookies().getFirst("XSRF-TOKEN"));
+
+        // xsrfTokenCookieOptは以下のいずれか。
+        //   a) Optional<null>
+        //   b) Optional<ResponseCookie>
+        // isPresent・・・Optional<ResponseCookie>の時はテスト成功、Optional<null>の時にテスト失敗。
+        //                ⇒ a)でないことを検証する。
+        // hasValueSatisfying・・・Optionalの中身：ResponseCookieを検証できる（この中身がラムダ式の引数で受け取れる）。
+        //                ⇒ 事前にisPresentすることで、ResponseCookieはオブジェクト自体は存在する。
+        assertThat(xsrfTokenCookieOpt)
+                .isPresent()
+                .hasValueSatisfying(
+                        xsrfTokenCookie-> assertThat(xsrfTokenCookie.getValue()).isNotBlank()
+                );
     }
 
 }

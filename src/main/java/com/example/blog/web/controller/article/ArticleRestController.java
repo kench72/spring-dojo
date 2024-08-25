9aefe02d73274d3b8fac1a357c47dc6bd525ee44
kench <77647366+kench72@users.noreply.github.com>
2024-08-25 10:46:34 +0900

    ResourceNotFoundExceptionの新設
    ExeceptionHandler導入による既存処理への影響対応。
    具体的には404エラー（ResponseStatusException(HttpStatus.NOT_FOUND)）としていた箇所が全て500エラーになってしまう問題を解消。

--- src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
+++ src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
@@ -1,12 +1,11 @@
 package com.example.blog.web.controller.article;
 
 import com.example.blog.service.article.ArticleService;
+import com.example.blog.web.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,8 +15,12 @@ public class ArticleRestController {
 
     @GetMapping("/articles/{id}")
     public ArticleDTO showArticle(@PathVariable("id") long id) {
+        // memo：メソッド参照
+        // 引数のないラムダ式はメソッド参照で定義可能。
+        // ResourceNotFoundException::new
+        // ::newだと引数のないデフォルトコンストラクタ
         return articleService.findById(id) // Optional<ArticleEntity>
                 .map(ArticleDTO::from)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND)); // Optional<ArticleDTO>
+                .orElseThrow(ResourceNotFoundException::new); // Optional<ArticleDTO>
     }
 }
